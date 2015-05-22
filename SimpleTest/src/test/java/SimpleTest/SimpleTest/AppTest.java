46e922bd7a8000cbcac2614ef66e9abd55b4c1ac
U-SYNAPSE\ruslan.kryvosheiev <ruslan.kryvosheiev@LWO1-DHP-F09815.synapse.com>
2015-05-22 14:01:28 +0300

    simple test

---
+++ SimpleTest/src/test/java/SimpleTest/SimpleTest/AppTest.java
@@ -0,0 +1,23 @@
+package SimpleTest.SimpleTest;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	
+	 @BeforeClass
+	 public void setUp() {
+	   // code that will be invoked when this test is instantiated
+	 }
+	 
+	 @Test(groups = { "fast" })
+	 public void aFastTest() {
+	   System.out.println("Fast test");
+	 }
+	 
+	 @Test(groups = { "slow" })
+	 public void aSlowTest() {
+	    System.out.println("Slow test");
+	 }
+	 
+	}
